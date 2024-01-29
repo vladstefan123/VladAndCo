5c650ed030e5ab19c4299467d6f172300dab7134
vladstefan123 <ionitavladstefan1@gmail.com>
2024-01-29 18:18:41 +0200

    sgrfvs

---
+++ 3.0/Assets/scripts/follow1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow1 : MonoBehaviour
+{
+     private Transform target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target= GameObject.FindGameObjectWithTag("MARIUS").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position= Vector2.MoveTowards(transform.position,target.position,speed* Time.deltaTime);
+    }
+}
---
+++ 3.0/Assets/scripts/playerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public int maxHealth=10;
+    public playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+      maxHealth=maxHealth;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
