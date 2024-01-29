805964aea955228fcad801075a3d5e99de290d19
vladstefan123 <ionitavladstefan1@gmail.com>
2024-01-29 19:35:40 +0200

    ma
    ma

--- 3.0/Assets/scripts/healthBar.cs
+++ 3.0/Assets/scripts/healthBar.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+    public playerHealth script;
+    void Start ()
     {
-        
+        script.currentHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public void SetHealth( script.currentHealth)
+{
+    slider.value = script.currentHealth;
+}
+
+    
+    
+    
+
 }
