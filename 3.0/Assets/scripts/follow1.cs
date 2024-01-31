5a987e0e587119465924659cd4f70de19e164219
flixzxc <dascalescugabriel01@gmail.com>
2024-01-31 21:07:27 +0200

    awawawa

--- 3.0/Assets/scripts/follow1.cs
+++ 3.0/Assets/scripts/follow1.cs
@@ -6,6 +6,7 @@ public class follow1 : MonoBehaviour
 {
      private Transform target;
     public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,19 @@ public class follow1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var rotationVectorSlime = transform.rotation.eulerAngles;
+        Quaternion rotationSlime = transform.rotation;
         transform.position= Vector2.MoveTowards(transform.position,target.position,speed* Time.deltaTime);
+        if(transform.position.x > target.position.x)
+        {
+            rotationVectorSlime.y = 180;
+            transform.rotation = Quaternion.Euler(rotationVectorSlime);
+        }
+        if (transform.position.x < target.position.x)
+        {
+            rotationVectorSlime.y = 0;
+            transform.rotation = Quaternion.Euler(rotationVectorSlime);
+        }
+
     }
 }
--- 3.0/Assets/scripts/playerHealth.cs
+++ 3.0/Assets/scripts/playerHealth.cs
@@ -18,8 +18,8 @@ public class playerHealth : MonoBehaviour
     
 public void TakeDamage(int damage)
 {
+
     currentHealth-=damage;
-   
     if(currentHealth <=0)
     {
         Destroy(gameObject);
