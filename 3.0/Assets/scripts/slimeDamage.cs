c03d714363f82e0b65492deff5a3314625fd54ab
flixzxc <dascalescugabriel01@gmail.com>
2024-02-01 17:26:42 +0200

    dasdasd

--- 3.0/Assets/scripts/slimeDamage.cs
+++ 3.0/Assets/scripts/slimeDamage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
